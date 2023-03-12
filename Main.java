7e006ed78f5bc2add985c9fe0756d082ec293348
Ilya <127449810+sumoomus@users.noreply.github.com>
2023-03-12 23:43:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+        public static void main(String[] args) {
+            int n = 135;
+            int sum = 0;
+            while (n != 0) {
+                sum = sum + n % 10;
+                n = n / 10;
+            }
+            System.out.println("sum of numbers:" + sum);
+        }
+    }
+
